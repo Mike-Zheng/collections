232ead98827a579297c046d8aa1eb7bd752df0ee
mikezh <liwenzheng.mike@gmail.com>
2022-07-11 21:49:22 +0800

    [add] run.wellness.suntory.com.tw

---
+++ run-wellness-suntory-com-tw/js/video.js
@@ -0,0 +1,30 @@
+﻿//youtube player
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player; 
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        width : '1280',
+        height : '720',
+        videoId : '',
+        events : {
+            'onReady' : onPlayerReady,
+            'onStateChange' : onPlayerStateChange
+        }
+    });
+}
+    
+function onPlayerReady(event) {
+    //event.target.playVideo();
+}
+function onPlayerStateChange(event) {
+    if(event.data == YT.PlayerState.ENDED) {
+        player.pauseVideo();
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
