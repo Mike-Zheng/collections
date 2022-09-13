8d23a8480062d177cea8cb34e8f65a9dabb96d0b
mike <liwenzheng.mike@gmail.com>
2022-09-13 10:01:31 +0800

    [add] flutterflow.io

---
+++ flutterflow.io/__/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBAj4NqmN3DYOubTxyJx6bTaQ0z0k5qw9U",
+  "appId": "1:51404331609:web:e6a087c54f0c6769db6f48",
+  "authDomain": "flutterflow-io-6f20.firebaseapp.com",
+  "databaseURL": "https://flutterflow-io-6f20.firebaseio.com",
+  "measurementId": "G-D6CBBKY5Y4",
+  "messagingSenderId": "51404331609",
+  "projectId": "flutterflow-io-6f20",
+  "storageBucket": "flutterflow-io-6f20.appspot.com"
+});
